b5a7d1d6f5985b4c61c7c71a3bc0fab6dbdc69d2
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-07-03 18:23:24 +0530

    day4

---
+++ day3Variables/index.js
@@ -0,0 +1,24 @@
+// what is variables understanding data types declaring and initialization variables
+
+// declaring ( let const var ) // initalization 
+
+// camelcase = userName
+// snakecase = snake_case// python
+
+heroName = "batman"//string
+
+let  heroPower = "billionare"//string
+console.log(heroPower)
+heroPower = "technology"
+
+console.log(heroName)
+console.log(heroPower)
+
+let number = 5// number
+
+console.log(number)
+
+
+let boolean = true// boolean = >true false
+
+console.log(boolean)
---
+++ day4datatypes/index.js
@@ -0,0 +1,93 @@
+// // data types
+// // Types Of Operator
+// // Arithmetic operators + * - /  **    (%=modules) return remainder
+// // Assignment operators // =  += -=  *= /=  **= 
+// // Comparison operators// < > <= >= == === != !==
+// // Logical operators// && and   || OR
+// // conversion with console.log and innerHTML // conversion how to convert data type 
+
+// //Data types
+
+// // Primitive
+
+// //string
+// // number
+// //bollean
+// //undefined
+// // null
+// //BigInt
+
+// // Non Primitive (reference type)
+// // array
+// // function
+// // object
+
+// let userName = "superman"; // "", '', `` string
+
+// console.log(typeof userName);
+
+// let value = 54;
+// let value2 = "54";
+
+// console.log(typeof value2);
+
+// console.log(typeof value);
+
+// let temp = true;
+// console.log(typeof typeof temp);
+
+// let hero;
+
+// console.log(typeof hero); // undefined
+
+// let game = null;
+// console.log(typeof game); // null data type object
+
+// let bigValue = 12309121309823090929834098234n; // (bigint value)
+
+// console.log(typeof bigValue);
+
+// // Non - primitive
+
+// let arr = [12, true, "superman", hero]; /// array is a collection mulitple data types //reference type ;
+
+// console.log(typeof arr);
+
+// // object is key value pair
+// let obj = {
+//   empName: "tony starke",
+//   age: 45,
+//   company: "stark industry",
+// };
+
+// console.log(typeof obj)
+// console.log(obj.empName)
+
+console.log("4");
+console.log("5");
+
+// output
+//5
+//3
+//1
+//2
+
+function show1() {
+  console.log("1");
+}
+
+function show2() {
+  console.log("2");
+}
+
+function show3() {
+  console.log("3");
+}
+
+show3(); // () parentesis help to call the  function
+
+show2();
+show1();
+
+
+
